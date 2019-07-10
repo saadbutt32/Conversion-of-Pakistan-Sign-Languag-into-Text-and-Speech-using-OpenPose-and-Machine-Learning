1d8fa779b7a293428b4cc73e1984ea2c6c902009
juni05 <junaidejaz05@gmail.com>
2019-07-10 15:31:53 +0500

    documentation

--- main2.py
+++ main2.py
@@ -10,27 +10,24 @@ Created on Thu Nov 22 22:38:14 2018
 
 import os
 import subprocess
-import ann_match
-import time
+import svm_match
 
 from pygame import mixer # for sound
-
-from tkinter import Tk
-from tkinter import Label
 
 # for error handling
 import errno, stat, shutil
 import sys, signal
-
+import eel
 
 """
 Handling Errors While removing temp folders
 """     
+eel.init("gui")
+
 def signal_handler(signal, frame):
     # Removing Keypoints Folder
     shutil.rmtree("Keypoints", ignore_errors=True, onerror=handleRemoveReadonly)
     os.system("taskkill /f /im  OpenPoseDemo.exe")
-    root.destroy()
     print( 'All done')
     sys.exit(0)
 
@@ -50,22 +47,12 @@ def handleRemoveReadonly(func, path, exc):
 shutil.rmtree("Keypoints", ignore_errors=True, onerror=handleRemoveReadonly)
 
 """
-Tkinter window
-"""
-root = Tk()
-root.geometry("300x300+1600+100") #Width x Height
-l = Label(root, text= "‬")
-l.config(font=("Courier", 100))
-l.pack()
-root.update()
-
-"""
 Starting OpenPoseDemo.exe
 and storing json files to temporary folder [Keypoints] 
 """
 print('Starting OpenPose')
 os.chdir('openpose')
-p = subprocess.Popen('build\\x64\\Release\\OpenPoseDemo.exe --hand  --write_json ..\\Keypoints --net_resolution 128x128  --number_people_max 1', shell=True)
+p = subprocess.Popen('bin\\OpenPoseDemo.exe --hand  --write_json ..\\Keypoints --net_resolution 128x128  --number_people_max 1', shell=True)
 os.chdir('..')
 
 """
@@ -87,6 +74,7 @@ except FileExistsError:
 Load each .json file from Keypoints folder and
 predict the label 
 """
+@eel.expose
 def match():
     label=''
     lastLabel = ''
@@ -95,37 +83,35 @@ def match():
             if os.path.splitext(entry)[1] == ".json":
                 fileName = entry.name     
     try:
-        label = ann_match.match_ann('Keypoints\\'+fileName) 
+       label = svm_match.match_svm('Keypoints\\'+fileName)
     except:
         pass
     
     if label != 'no match' and label != 'no confidence' and label != lastLabel:
         lastLabel = label  
         
-        l[ "text" ]=label
-        root.update()
         mp3 = "speech\\"+label+".mp3"
         mixer.init()
         mixer.music.load(mp3)
         mixer.music.play()
         return label
         
-        
+
+eel.start('sign.html',size=(1000,600))        
 
 label=""
 while True:
+        
 #    time.sleep(0.5)
 #    try:
 
         label = match()
         if label!=None and label != "":
             print(label)   
-
 #    except UnboundLocalError:
 #        print("UnboundLocalError")
     
- 
-root.mainloop()
+
 
 
 
